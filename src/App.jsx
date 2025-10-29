a081f502c93385eb9f3bfe4486b45328076273e1
bardia-asd <brdyaasdy5@gmail.com>
2025-10-30 01:44:52 +0330

    chore: install lucide-react icon library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
 
 function App() {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
-    );
+    return <></>;
 }
 
 export default App;
